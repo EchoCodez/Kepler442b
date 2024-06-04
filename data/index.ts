7221a19b8850364345ba5ba90b8c7ac0eac06ad5
EchoCodez <mihir.nimkar@gmail.com>
2024-06-03 20:24:52 -0400

    amazing quote

--- data/index.ts
+++ data/index.ts
@@ -95,9 +95,9 @@ export const testimonials = [
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "The natural resources and starlight on Kepler 442b make it a perfect candidate for life supported by hydroponics based agriculture and innovative water filtration. I would love to live there!",
     name: "Shruti Bedekar",
-    title: "Straight A+ Student at Briar Woods HS and Professional Dancer",
+    title: "Straight A+ Student at Briar Woods HS and Professional Singer",
   },
 ];
 
